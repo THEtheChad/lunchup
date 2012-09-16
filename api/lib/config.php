92eeae765b3d808d9ef7b2219ecd0c8706651a43
Bishop81 <bishop81@gmail.com>
2012-09-15 19:58:45 -0500

    change db to localhost

--- api/lib/config.php
+++ api/lib/config.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('UTC');
 
-ORM::configure('mysql:host=174.120.195.62:3306;dbname=bishop81_lunchup2');
+ORM::configure('mysql:host=localhost;dbname=bishop81_lunchup2');
 ORM::configure('username', 'bishop81_lunchup');
 ORM::configure('password', '5X#2+L5,g=Jb');
 
